acfb64fe3b12457cd3e2dcf908c7b6287ff0a116
Umme Athiya <43459908+UMMEATHIYA@users.noreply.github.com>
2025-07-23 13:41:34 -0500

    valid palindrome added

---
+++ Two Pointers/valid_Palindrome.py
@@ -0,0 +1,16 @@
+def isPalindrome(s):
+    i, j = 0, len(s) - 1
+    while i < j:
+        while i < j and not s[i].isalnum():
+            i += 1
+        while i < j and not s[j].isalnum():
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i += 1
+        j -= 1
+    return True
+
+# Example input
+s = "A man, a plan, a canal: Panama"
+print(isPalindrome(s))
